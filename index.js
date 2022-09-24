c14d927967be79b9633a43727bb657925c01c5d7
Nicole Robbins <nicole.robbins@att.net>
2022-09-23 21:01:57 -0500

    commit tree 087504843c6fba380edf01894ff3ed0034b13d68

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const newCats = [...cats, name];
+    return newCats;
+}
+function prependCat(name){
+    const otherCats = [name, ...cats];
+    return otherCats;
+}
+function removeLastCat(){
+    const noGarfield = cats.slice(0, cats.length-1);
+    return noGarfield;
+}
+function removeFirstCat(){
+    const noMilo = cats.slice(1);
+    return noMilo;
+}
+
